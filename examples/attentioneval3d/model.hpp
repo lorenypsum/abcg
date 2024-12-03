2b640fc0f99c64852321a71d12ae5262f49fd4c8
lorenypsum <loren_ypsum@gmail.com>
2024-12-03 15:33:41 -0300

    xc

--- examples/attentioneval3d/model.hpp
+++ examples/attentioneval3d/model.hpp
@@ -7,17 +7,20 @@ struct Vertex {
   glm::vec3 position{};
   glm::vec3 normal{};
   glm::vec2 texCoord{};
+  glm::vec4 tangent{};
   friend bool operator==(Vertex const &, Vertex const &) = default;
 };
 
 class Model {
 public:
+  void loadCubeTexture(std::string const &path);
   void loadDiffuseTexture(std::string_view path);
+  void loadNormalTexture(std::string_view path);
   void loadObj(std::string_view path, bool standardize = true);
   void render(int numTriangles = -1) const;
   void setupVAO(GLuint program);
   void setSize(float size) { m_size = size; };
-  void destroy() const;
+  void destroy();
 
   [[nodiscard]] int getNumTriangles() const {
     return gsl::narrow<int>(m_indices.size()) / 3;
@@ -29,6 +32,7 @@ public:
   [[nodiscard]] float getShininess() const { return m_shininess; }
 
   [[nodiscard]] bool isUVMapped() const { return m_hasTexCoords; }
+  [[nodiscard]] GLuint getCubeTexture() const { return m_cubeTexture; }
 
 private:
   GLuint m_VAO{};
@@ -40,6 +44,8 @@ private:
   glm::vec4 m_Ks{};
   float m_shininess{};
   GLuint m_diffuseTexture{};
+  GLuint m_normalTexture{};
+  GLuint m_cubeTexture{};
 
   std::vector<Vertex> m_vertices;
   std::vector<GLuint> m_indices;
@@ -47,8 +53,9 @@ private:
 
   bool m_hasNormals{false};
   bool m_hasTexCoords{false};
-  
+
   void computeNormals();
+  void computeTangents();
   void createBuffers();
   void standardize();
 };
