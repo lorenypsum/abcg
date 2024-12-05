eaf40697aab76dabf42cab678feeed19a9960850
lorenypsum <loren_ypsum@gmail.com>
2024-12-05 20:07:32 -0300

    a

--- examples/attentioneval3d/game_objects.cpp
+++ examples/attentioneval3d/game_objects.cpp
@@ -4,21 +4,23 @@
 #include <glm/gtx/fast_trigonometry.hpp>
 #include <iostream>
 
-// Cria objetos
+// Cria Entidades do Jogo
 void GameEntities::create() {
-  auto const assetsPath{abcg::Application::getAssetsPath()};
+  auto const assetsPath{
+      abcg::Application::getAssetsPath()}; // Caminho dos assets
 
-  abcg::glEnable(GL_DEPTH_TEST);
+  abcg::glEnable(GL_DEPTH_TEST); // Habilita teste de profundidade
 
-  m_program =
-      abcg::createOpenGLProgram({{.source = assetsPath + "shaders/texture.vert",
-                                  .stage = abcg::ShaderStage::Vertex},
-                                 {.source = assetsPath + "shaders/texture.frag",
-                                  .stage = abcg::ShaderStage::Fragment}});
+  m_program = abcg::createOpenGLProgram(
+      {{.source = assetsPath + "shaders/texture.vert",
+        .stage = abcg::ShaderStage::Vertex},
+       {.source = assetsPath + "shaders/texture.frag",
+        .stage = abcg::ShaderStage::Fragment}}); // Programa de shader
 
   createObject(m_distractionModel, assetsPath, "bird.obj",
-               "maps/b-feathers.png");
-  createObject(m_targetModel, assetsPath, "bird.obj", "maps/w-feathers.png");
+               "maps/b-feathers.png"); // Cria objeto de distração
+  createObject(m_targetModel, assetsPath, "bird.obj",
+               "maps/w-feathers.png"); // Cria objeto alvo
 
   setupSceneObjects(m_distractionObjects, // m_sceneObjects
                     -15.0f,               // minX
@@ -27,7 +29,8 @@ void GameEntities::create() {
                     15.0f,                // maxY
                     -50.0f,               // minZ
                     0.0f                  // maxZ
-  );
+  );                                      // Configura objetos de distração
+
   setupSceneObjects(m_targetObjects, // m_sceneObjects
                     -15.0f,          // minX
                     15.0f,           // maxX
@@ -35,157 +38,192 @@ void GameEntities::create() {
                     15.0f,           // maxY
                     -50.0f,          // minZ
                     0.0f             // maxZ
-  );
+  );                                 // Configura objetos alvo
 }
 
+// Configura objetos de cena
 void GameEntities::setupSceneObjects(std::vector<SceneObject> &m_sceneObjects,
                                      float minX, float maxX, float minY,
                                      float maxY, float minZ, float maxZ) {
-  // Setup scene objects
   for (auto &sceneObject : m_sceneObjects) {
     randomizeSceneObject(sceneObject, minX, maxX, minY, maxY, minZ, maxZ);
   }
 }
 
+// Atualiza objetos de cena
 void GameEntities::updateSceneObjects(std::vector<SceneObject> &m_sceneObjects,
                                       float deltaTime, float incZ, float incX,
                                       float incY, float posZ, float minX,
                                       float maxX, float minY, float maxY,
                                       float minZ, float maxZ) {
 
-  m_angle = glm::wrapAngle(m_angle + glm::radians(90.0f) * deltaTime);
+  m_angle = glm::wrapAngle(m_angle + glm::radians(90.0f) * deltaTime); // Ângulo
 
-  // Update scene objects
+  // Atualiza posição de cada objeto de cena
   for (auto &sceneObject : m_sceneObjects) {
-    // Increase z by incZ units per second
+
+    // Atualiza posição do objeto pelo tempo
     sceneObject.m_position.z += deltaTime * incZ;
     sceneObject.m_position.x += deltaTime * incX;
     sceneObject.m_position.y += deltaTime * incY;
 
-    // If this sceneObject is behind the camera, select a new random position
+    // Seleciona nova posição aleatória caso o objeto esteja fora da tela
     if (sceneObject.m_position.z > 0.1f) {
       randomizeSceneObject(sceneObject, minX, maxX, minY, maxY, minZ, maxZ);
-      sceneObject.m_position.z = posZ; // Back to -100
+      // Volta para a posição passada por parâmento (-100 por exemplo)
+      sceneObject.m_position.z = posZ;
     }
   }
 }
 
+// Cria um objeto de cena
 void GameEntities::createObject(Model &m_model, const std::string &assetsPath,
                                 const std::string &objPath,
                                 const std::string &texturePath) {
-  m_model.loadDiffuseTexture(assetsPath + texturePath);
-  m_model.loadObj(assetsPath + objPath);
-  m_model.setSize(100.0f);
-  m_model.setupVAO(m_program);
 
-  m_Ka = m_model.getKa();
-  m_Kd = m_model.getKd();
-  m_Ks = m_model.getKs();
-  m_shininess = m_model.getShininess();
+  // Carrega modelo, textura, tamanho e configura VAO
+  m_model.loadDiffuseTexture(assetsPath + texturePath); // Textura
+  m_model.loadObj(assetsPath + objPath);                // Objeto
+  m_model.setSize(100.0f);                              // Tamanho
+  m_model.setupVAO(m_program);                          // Configura VAO
 
-  // Camera at (0,0,0) and looking towards the negative z
-  glm::vec3 const eye{0.0f, 0.0f, 0.0f};
-  glm::vec3 const at{0.0f, 0.0f, -1.0f};
-  glm::vec3 const up{0.0f, 1.0f, 0.0f};
-  m_viewMatrix = glm::lookAt(eye, at, up);
+  // Variáveis uniformes do modelo
+  m_Ka = m_model.getKa();               // Coeficiente de reflexão ambiente
+  m_Kd = m_model.getKd();               // Coeficiente de reflexão difusa
+  m_Ks = m_model.getKs();               // Coeficiente de reflexão especular
+  m_shininess = m_model.getShininess(); // Brilho
+
+  // Camera em posição inicial
+  glm::vec3 const eye{0.0f, 0.0f, 0.0f}; // Posição da câmera (origem)
+  glm::vec3 const at{0.0f, 0.0f,
+                     -1.0f}; // Ponto que câmera está olhando (eixo z)
+  glm::vec3 const up{0.0f, 1.0f, 0.0f};    // Vetor up da câmera (eixo y)
+  m_viewMatrix = glm::lookAt(eye, at, up); // Matriz de visão
 }
 
+// Aleatoriza a posição de um objeto de cena
 void GameEntities::randomizeSceneObject(SceneObject &sceneObject, float minX,
                                         float maxX, float minY, float maxY,
                                         float minZ, float maxZ) {
-  // Random position: x and y in [minX, maxX), z in [minZ, maxZ)
-  std::uniform_real_distribution<float> distPosX(minX, maxX);
-  std::uniform_real_distribution<float> distPosY(minY, maxY);
-  std::uniform_real_distribution<float> distPosZ(minZ, maxZ);
-  sceneObject.m_position =
-      glm::vec3(distPosX(m_randomEngine), distPosY(m_randomEngine),
-                distPosZ(m_randomEngine));
 
-  // Random rotation axis
-  sceneObject.m_rotationAxis = glm::sphericalRand(1.0f);
+  // Posição aleatória no espaço 3D
+  std::uniform_real_distribution<float> distPosX(
+      minX, maxX); // Distribuição de posição x
+  std::uniform_real_distribution<float> distPosY(
+      minY, maxY); // Distribuição de posição y
+  std::uniform_real_distribution<float> distPosZ(
+      minZ, maxZ); // Distribuição de posição z
+  sceneObject.m_position =
+      glm::vec3(distPosX(m_randomEngine), // Posição aleatória no eixo x
+                distPosY(m_randomEngine), // Posição aleatória no eixo y
+                distPosZ(m_randomEngine)  // Posição aleatória no eixo z
+      );
+
+  sceneObject.m_rotationAxis = glm::sphericalRand(1.0f); // Eixo de rotação
 }
 
+// Renderiza objetos de cena
 void GameEntities::paint() {
-  abcg::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  abcg::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Limpa a tela
 
-  abcg::glViewport(0, 0, m_viewportSize.x, m_viewportSize.y);
+  abcg::glViewport(0, 0, m_viewportSize.x, m_viewportSize.y); // Viewport
 
-  abcg::glUseProgram(m_program);
+  abcg::glUseProgram(m_program); // Usa o programa de shader
 
-  // Get location of uniform variables
-  auto const viewMatrixLoc{abcg::glGetUniformLocation(m_program, "viewMatrix")};
-  auto const projMatrixLoc{abcg::glGetUniformLocation(m_program, "projMatrix")};
+  // Atribui variáveis uniformes
+  auto const viewMatrixLoc{
+      abcg::glGetUniformLocation(m_program, "viewMatrix")}; // Matriz de visão
+  auto const projMatrixLoc{abcg::glGetUniformLocation(
+      m_program, "projMatrix")}; // Matriz de projeção
   auto const modelMatrixLoc{
-      abcg::glGetUniformLocation(m_program, "modelMatrix")};
+      abcg::glGetUniformLocation(m_program, "modelMatrix")}; // Matriz de modelo
   auto const normalMatrixLoc{
-      abcg::glGetUniformLocation(m_program, "normalMatrix")};
+      abcg::glGetUniformLocation(m_program, "normalMatrix")}; // Matriz normal
   auto const lightDirLoc{
-      abcg::glGetUniformLocation(m_program, "lightDirWorldSpace")};
-  auto const shininessLoc{abcg::glGetUniformLocation(m_program, "shininess")};
+      abcg::glGetUniformLocation(m_program, "lightDirWorldSpace")}; // Luz
+  auto const shininessLoc{
+      abcg::glGetUniformLocation(m_program, "shininess")}; // Brilho
+  auto const IaLoc{
+      abcg::glGetUniformLocation(m_program, "Ia")}; // Intensidade ambiente
+  auto const IdLoc{
+      abcg::glGetUniformLocation(m_program, "Id")}; // Intensidade difusa
+  auto const IsLoc{
+      abcg::glGetUniformLocation(m_program, "Is")}; // Intensidade especular
+  auto const KaLoc{
+      abcg::glGetUniformLocation(m_program, "Ka")}; // Coeficiente ambiente
+  auto const KdLoc{
+      abcg::glGetUniformLocation(m_program, "Kd")}; // Coeficiente difuso
+  auto const KsLoc{
+      abcg::glGetUniformLocation(m_program, "Ks")}; // Coeficiente especular
+  auto const diffuseTexLoc{
+      abcg::glGetUniformLocation(m_program, "diffuseTex")}; // Textura
+  auto const lightDirRotated{
+      glm::vec4(0.0f, 0.0f, 1.0f, 1.0f)}; // Luz rotacionada
 
-  auto const IaLoc{abcg::glGetUniformLocation(m_program, "Ia")};
-  auto const IdLoc{abcg::glGetUniformLocation(m_program, "Id")};
-  auto const IsLoc{abcg::glGetUniformLocation(m_program, "Is")};
-  auto const KaLoc{abcg::glGetUniformLocation(m_program, "Ka")};
-  auto const KdLoc{abcg::glGetUniformLocation(m_program, "Kd")};
-  auto const KsLoc{abcg::glGetUniformLocation(m_program, "Ks")};
-  auto const diffuseTexLoc{abcg::glGetUniformLocation(m_program, "diffuseTex")};
+  // Aplica variáveis uniformes
+  abcg::glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE,
+                           &m_viewMatrix[0][0]); // Matriz de visão
+  abcg::glUniformMatrix4fv(projMatrixLoc, 1, GL_FALSE,
+                           &m_projMatrix[0][0]); // Matriz de projeção
+  abcg::glUniform1i(diffuseTexLoc, 0);           // Textura
+  abcg::glUniform4fv(lightDirLoc, 1, &lightDirRotated.x); // Luz
+  abcg::glUniform4fv(IaLoc, 1, &m_Ia.x); // Intensidade ambiente
+  abcg::glUniform4fv(IdLoc, 1, &m_Id.x); // Intensidade difusa
+  abcg::glUniform4fv(IsLoc, 1, &m_Is.x); // Intensidade especular
 
-  // Set uniform variables that have the same value for every model
-  abcg::glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, &m_viewMatrix[0][0]);
-  abcg::glUniformMatrix4fv(projMatrixLoc, 1, GL_FALSE, &m_projMatrix[0][0]);
-  abcg::glUniform1i(diffuseTexLoc, 0);
-
-  auto const lightDirRotated{glm::vec4(0.0f, 0.0f, 1.0f, 1.0f)};
-
-  abcg::glUniform4fv(lightDirLoc, 1, &lightDirRotated.x);
-  abcg::glUniform4fv(IaLoc, 1, &m_Ia.x);
-  abcg::glUniform4fv(IdLoc, 1, &m_Id.x);
-  abcg::glUniform4fv(IsLoc, 1, &m_Is.x);
-
+  // Renderiza objetos de cena
   renderObject(m_distractionModel, KaLoc, KdLoc, KsLoc, shininessLoc,
                modelMatrixLoc, m_distractionObjects);
   renderObject(m_targetModel, KaLoc, KdLoc, KsLoc, shininessLoc, modelMatrixLoc,
                m_targetObjects);
 
+  // Finaliza
   abcg::glUseProgram(0);
 }
 
+// Renderiza objetos de cena
 void GameEntities::renderObject(Model &m_model, const GLint KaLoc,
                                 const GLint KdLoc, const GLint KsLoc,
                                 const GLint shininessLoc,
                                 const GLint modelMatrixLoc,
-                                std::vector<SceneObject> &m_sceneObjects) {
-  // Set uniform variables for the current model
+                                std::vector<SceneObject> &m_scObjects) {
+
+  // Aplica variáveis uniformes para o modelo de matriz de protótipo
   glm::mat4 modelMatrixProt{1.0f};
+  abcg::glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE,
+                           &modelMatrixProt[0][0]); // Matriz de modelo
+  abcg::glUniform4fv(KaLoc, 1, &m_Ka.x);            // Coeficiente ambiente
+  abcg::glUniform4fv(KdLoc, 1, &m_Kd.x);            // Coeficiente difuso
+  abcg::glUniform4fv(KsLoc, 1, &m_Ks.x);            // Coeficiente especular
+  abcg::glUniform1f(shininessLoc, m_shininess);     // Brilho
 
-  abcg::glUniform4fv(KaLoc, 1, &m_Ka.x);
-  abcg::glUniform4fv(KdLoc, 1, &m_Kd.x);
-  abcg::glUniform4fv(KsLoc, 1, &m_Ks.x);
-  abcg::glUniform1f(shininessLoc, m_shininess);
+  // Renderiza cada objeto de cena
+  for (auto &scObject : m_scObjects) {
 
-  // Set uniform variable
-  abcg::glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &modelMatrixProt[0][0]);
-
-  // Render each sceneObject
-  for (auto &sceneObject : m_sceneObjects) {
-    // Compute model matrix of the current sceneObject
+    // Computa a matriz de modelo para todos objeto de cena
     glm::mat4 modelMatrix{1.0f};
-    modelMatrix = glm::translate(modelMatrix, sceneObject.m_position);
-    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.7f));
-    modelMatrix = glm::rotate(modelMatrix, m_angle, sceneObject.m_rotationAxis);
 
-    // Set uniform variable
+    modelMatrix =
+        glm::translate(modelMatrix, scObject.m_position); // Posição do objeto
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.7f)); // Escala do objeto
+    modelMatrix = glm::rotate(modelMatrix, m_angle,
+                              scObject.m_rotationAxis); // Rotação do objeto
+
+    // Aplica variáveis uniformes para o modelo
     abcg::glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &modelMatrix[0][0]);
     m_model.render();
   }
 }
 
+// Verifica se um objeto de cena foi clicado
 bool GameEntities::checkClickOnObject(
-    glm::vec3 const &clickPos, glm::mat4 const &viewMatrix,
-    glm::mat4 const &projMatrix, std::vector<SceneObject> &m_sceneObjects) {
+    glm::vec3 const &clickPos,               // clickPos
+    glm::mat4 const &viewMatrix,             // viewMatrix
+    glm::mat4 const &projMatrix,             // projMatrix
+    std::vector<SceneObject> &m_sceneObjects // m_sceneObjects
+) {
   for (auto &sceneObject : m_sceneObjects) {
-    // Posição 3D da estrela
+    // Posição 3D do objeto de cena (pássaro)
     glm::vec3 sceneObjectPosition = sceneObject.m_position;
 
     // Converta a posição do objeto de cena para o espaço da tela
